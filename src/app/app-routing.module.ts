a95e8146583b5f23199f1238d97fdf2cdd24ad34
hervegithub <herventonga20@gmail.com>
2020-08-15 14:36:48 +0100

    Update project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { BoardComponent } from './board/board.component';
 
 
 
 const routes: Routes = [
-  {
-    path: "",
-    component: LoginComponent,
-  }
+  {path: "", component: LoginComponent},
+  {path: "board", component: BoardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import {MatButtonModule} from "@angular/material/button"
+import { HeaderComponent } from './header/header.component'
+
+
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from '@angular/material/input';
+import { BoardComponent } from './board/board.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    BoardComponent
   ],
   imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
     MatButtonModule,
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
