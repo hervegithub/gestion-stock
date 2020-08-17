1e4866770ed8eead33f4d0b93f5d01b2b709d2ff
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2020-08-17 14:31:49 +0100

    Ajout du menu de navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from '@angular/material/input';
 import { BoardComponent } from './board/board.component';
 import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator"
+import {MatPaginatorModule} from "@angular/material/paginator";
+
 
 
 @NgModule({
