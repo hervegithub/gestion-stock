612be3bc9c8b8cb86bba48c399c0b012898a70bb
hervegithub <herventonga20@gmail.com>
2020-08-17 18:10:22 +0100

    Adding new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BoardComponent } from './board/board.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
 
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
-  {path: "board", component: BoardComponent}
+  {path: "board", component: BoardComponent},
+  {path: "new-product", component: NewProductComponent}
 ];
 
 @NgModule({
