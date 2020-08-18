ef81bc0146aea33ab73784732b6002cbc6413d22
hervegithub <herventonga20@gmail.com>
2020-08-18 14:49:08 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BoardComponent } from './board/board.component';
 import { NewProductComponent } from './new-product/new-product.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
+  {path: "register", component:RegisterComponent},
   {path: "board", component: BoardComponent},
   {path: "new-product", component: NewProductComponent}
 ];
